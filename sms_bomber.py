47e52386074232bc724398d6e44e3abb994579ef
Sachin Singh Negi <negisachin688@gmail.com>
2020-04-01 12:09:03 +0530

    Update sms_bomber.py

--- sms_bomber.py
+++ sms_bomber.py
@@ -3,7 +3,7 @@ import time
 driver="D:/Selenium/env/Scripts/chromedriver.exe" #address of the chrome driver
 browser=webdriver.Chrome(driver)
 frequency=14
-mobile_no="8193037318" #target number
+mobile_no="1234567890" #target number
 for i in range(frequency):
     browser.get("https://www.flipkart.com/account/login?ret=/")
     number=browser.find_element_by_class_name("_2zrpKA")
